bca4bfc519a2ea9a54535627b506fddd93b0efeb
Michał Krzysztof Feiler <archiet@platinum.edu.pl>
2018-05-14 08:12:17 +0200

    some spaces

--- bdrzewa-traverse.c
+++ bdrzewa-traverse.c
@@ -1,11 +1,11 @@
-struct traversal begin_traversal(	struct traversal t,
-					unsigned copy	) {
+struct traversal begin_traversal(    struct traversal t,
+                                     unsigned copy	) {
 	struct traversal r = t;
 	size_t much = sizeof(struct location)*r.depth;
 	struct location l = r.loc[r.depth-1];
 	if(!((struct node*)(l.which->s[l.where]))->is_leaf) {
-		if(copy) { 	r.loc = malloc(much);
-				memcpy(r.loc, t.loc, much);	}
+		if(copy) {    r.loc = malloc(much);
+		              memcpy(r.loc, t.loc, much);	}
 		much+=sizeof(struct location);
 		r.loc = realloc(r.loc, much);
 		r.loc[r.depth].which = l.which->s[l.where];
