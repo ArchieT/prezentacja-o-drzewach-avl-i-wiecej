67e42d467e4958f11f599c59e9402284b1149307
Michał Krzysztof Feiler <archiet@platinum.edu.pl>
2018-05-14 06:31:15 +0200

    no equals sign in defines

--- bdrzewa-impl1.c
+++ bdrzewa-impl1.c
@@ -1,9 +1,9 @@
-#define E = 1
-#define EM = (E + 1)
-#define D = (1 << E)
-#define DP = (D + 1)
-#define DM = (1 << EM)
-#define DMP = (DM + 1)
+#define E 1
+#define EM (E + 1)
+#define D (1 << E)
+#define DP (D + 1)
+#define DM (1 << EM)
+#define DMP (DM + 1)
 typedef struct Val {
 	short unsigned len;
 	char *s;
